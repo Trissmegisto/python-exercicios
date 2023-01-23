8d31df350d90dc36bc8c136a45d8852f0d0ff3c6
Trissmegisto <84871727+Trissmegisto@users.noreply.github.com>
2023-01-23 12:38:05 -0300

    Update jogo de adivinhação.py
    Jogo da adivinhação para processo inicial no aprendizado de python. Foram adicionados níveis, biblioteca random, condição de repetição com for, condições como if, else e elif, função int e abs.

--- jogo de adivinhação.py
+++ jogo de adivinhação.py
@@ -1,18 +1,52 @@
-print("*******************")
-print("JOGO DE ADIVINHAÇÃO")
-print("*******************")
+import random
 
-numero_secreto = 32
+pontos = 1000
 
-chute = input("Digite seu número no console:  ")
+print("JOGO DA ADIVINHAÇÃO")
 
-chute= int(chute)
+numero_secreto = random.randrange(1, 101)
+tentativas = 3
+rodadas = 1
 
-print("Você digitou" , chute)
+print("Qual nível de dificuldade?")
+print("(1) Fácil")
+print("(2) Médio")
+print("(3) Difícil")
 
-if(numero_secreto == chute):
-    print("Você acertou!")
+nivel = int(input("Defina o nível: "))
+
+if(nivel == 1):
+    tentativas = 20
+elif(nivel == 2):
+    tentativas = 10
 else:
-    print("Você errou!")
+    tentativas = 5
 
-print("Fim do jogo.")
+
+for rodadas in range(1, tentativas + 1):
+    print("Tentativas {} de {}".format(rodadas, tentativas))
+    chute_str = input("Chute um número de 1 a 100: ")
+    print("Você chutou o número", chute_str)
+    chute = int(chute_str)
+
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+    acerto = chute == numero_secreto
+
+    if (chute > 100 or chute < 1):
+        print("Deve ser um número de 1 a 100!!")
+        continue
+    else:
+        if (acerto):
+            print("Você acertou :)")
+            break
+        else:
+            if (maior):
+                print("Você chutou um número maior que o número secreto.")
+            elif (menor):
+                print("Você chutou um número menor que o número secreto.")
+
+        pontos_perdidos = abs(chute - numero_secreto)
+        pontos = pontos - pontos_perdidos
+ 
+print("Fim do jogo! O número certo é: ", numero_secreto, "e sua pontuação foi: ", pontos)
